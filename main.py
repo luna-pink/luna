a04a45594abc66d418ba762e0b7d177e9392ebac
nshout <nshout99@gmail.com>
2023-09-03 20:14:57 +0200

    Add main file for Luna selfbot
    This commit adds the main file main.py for the Luna selfbot. The script utilizes python's argparse library to parse commandline arguments for different functionalities like running the CLI version of Luna, printing its version, and enabling verbose logging. It is important to note the if __name__ == '__main__': allows the script to be run standalone or imported as a module.

---
+++ main.py
@@ -0,0 +1,44 @@
+#    __         __  __     __   __     ______
+#   /\ \       /\ \/\ \   /\ "-.\ \   /\  __ \
+#   \ \ \____  \ \ \_\ \  \ \ \-.  \  \ \  __ \
+#    \ \_____\  \ \_____\  \ \_\\"\_\  \ \_\ \_\
+#     \/_____/   \/_____/   \/_/ \/_/   \/_/\/_/
+
+"""luna selfbot main file."""
+
+import argparse
+
+
+def main():
+    """
+    The main function of Luna.
+    :return:
+    """
+
+    parser = argparse.ArgumentParser(
+        description='Luna selfbot, one step ahead of Discord.'
+    )
+
+    parser.add_argument(
+        '-c',
+        '--cli',
+        action='store_true',
+        help='Run the CLI version of Luna.'
+    )
+
+    parser.add_argument(
+        '-v',
+        '--version',
+        action='store_true',
+        help='Print the version of Luna.'
+    )
+
+    parser.add_argument(
+        '--verbose',
+        action='store_true',
+        help='Enable verbose logging.'
+    )
+
+
+if __name__ == '__main__':
+    main()
