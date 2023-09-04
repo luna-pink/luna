8ff567abf178857b7bc1e72020dba2a99804b75e
nshout <nshout99@gmail.com>
2023-09-04 18:57:00 +0200

    Improve UI component management and settings application
    Introduced new structures 'view_base' and 'view_container' to enhance the management of graphical user interface components in 'viewport.py'. By specifying image attributes and behaviors within these structures, the code is more maintainable and navigable. Additionally, streamlined settings application by removing the necessity for creating an InterfaceSettings instance — settings are now directly applied to the components within 'viewport.py'. These changes aim to boost code efficiency and simplify future developments.

--- gui/components/viewport.py
+++ gui/components/viewport.py
@@ -9,11 +9,41 @@ import config
 
 components = config.Components([]).components
 
+view_base = flet.Container(
+    # image_src="background.png",
+    blur=3,
+    image_fit=flet.ImageFit.COVER,
+    border_radius=8,
+    expand=True,
+)
+components.extend(
+    [
+        [view_base, str, "image_src", "view_base", "image_url"],
+        [view_base, str, "gradient", "view_base", "color"],
+        [view_base, [int, None], "blur", "viewport", "blur"]
+    ]
+)
+
+view_container = flet.Container(
+    content=view_base,
+    blur=None,
+    image_fit=flet.ImageFit.COVER,
+    border_radius=8,
+    expand=True,
+    padding=8,
+)
+components.extend(
+    [
+        [view_container, [int, None], "blur", "viewport", "blur"],
+    ]
+)
+
 base = flet.Container(
     flet.Row(
         [
             flet.Row(
                 [
+                    view_container
                 ],
                 expand=True,
                 alignment=flet.MainAxisAlignment.CENTER,
@@ -26,14 +56,15 @@ base = flet.Container(
         spacing=0
     ),
     alignment=flet.alignment.center,
-    image_src="background.png",
+    # image_src="background.png",
+    image_src="https://wallpaperaccess.com/full/8351171.gif",
     image_fit=flet.ImageFit.COVER,
     expand=True,
     margin=0,
 )
 components.extend(
     [
-        [base, "image_src", "background", "image_url"],
-        [base, "gradient", "background", "color"]
+        [base, str, "image_src", "base", "image_url"],
+        [base, str, "gradient", "base", "color"]
     ]
 )
